359092bed1ed5faf9d7f274d7b8adbea4bf29c85
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2016-11-05 19:37:45 -0200

    Lets break stuff

--- run.php
+++ run.php
@@ -1,5 +1,10 @@
 <?php
 
+$content = file_get_contents("php://input");
+$update = json_decode($content, true);
+
+var_dump($update);die();
+
 include __DIR__.'/vendor/autoload.php';
 
 $app = new \Cilex\Application('Cilex');
